7fbe55cdf631018f6ff9031199af4fd6359d0f9f
vinicio5oares <vinicio.profissional@outlook.com>
2023-03-21 20:16:09 -0300

    first commit

---
+++ src/br/com/dio/PrimeroPrograma.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+public class PrimeroPrograma {
+    public static void main(String[] args) {
+
+
+
+        gato Gato = new gato();
+
+        System.out.println(Gato);
+       /*int a = 5;
+       int b = 3;
+        System.out.println("Hello World!" + (a+b));*/
+    }
+
+}
